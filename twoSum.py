b8b286d1cc2d4326a07c5aac722611a5c8f82694
niranjana-2004 <niranjana051104@gmail.com>
2026-01-07 20:19:37 +0530

    did an easy qn1 from leetcode

---
+++ twoSum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return [i,j]
+nums=list(map(int,input().split()))
+target=int(input())
+obj=Solution()
+print(obj.twoSum(nums,target))
