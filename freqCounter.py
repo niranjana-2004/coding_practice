507c5b0446cc2362a527491b2de4f16daaab40d1
niranjana-2004 <niranjana051104@gmail.com>
2026-05-29 14:25:52 +0530

    change in code

--- freqCounter.py
+++ freqCounter.py
@@ -1,5 +1,11 @@
 s=input()
 freq={}
+max_ch=""
+max_count=0
 for ch in s:
     freq[ch]=freq.get(ch,0)+1
+    if freq[ch]>max_count:
+        max_count=freq[ch]
+        max_ch=ch
 print(freq)
+print("Character ",max_ch,"occurred the most ","with ",max_count,"times")
