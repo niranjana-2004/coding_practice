68a0d46b24657d47ded4c9fc09bb764f21f13e88
niranjana-2004 <niranjana051104@gmail.com>
2026-05-29 12:38:36 +0530

    min and max in a list

--- minmax.py
+++ minmax.py
@@ -1,14 +1,9 @@
-class Solution:
-    def getMinMax(self,arr):
-        minimum=arr[0]
-        maximum=arr[0]
-        for i in range(1,len(arr)):
-            if arr[i]>maximum:
-                maximum=arr[i]
-            if arr[i]<minimum:
-                minimum=arr[i]
-        return maximum,minimum
 arr=list(map(int,input().split()))
-obj=Solution()
-res=obj.getMinMax(arr)
-print(res)
+min=arr[0]
+max=arr[0]
+for i in range(len(arr)):
+    if arr[i]<min:
+        min=arr[i]
+    if arr[i]>max:
+        max=arr[i]
+print(max,min)
