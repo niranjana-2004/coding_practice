2ba80991082905b7dc253213830ead1ce20f18b4
niranjana-2004 <niranjana051104@gmail.com>
2026-05-29 12:28:11 +0530

    multiplication table

--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,4 @@
-def multiplicationTable(n):
-    for i in range(1,11):
-        print(i*n)
 n=int(input())
-multiplicationTable(n)
+print("Multiplication Table of",n,":")
+for i in range(1,11):
+    print(i,"*",n,"=",i*n)
